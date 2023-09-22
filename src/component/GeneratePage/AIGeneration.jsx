2fcd0ca5382f83406dff3299d00aa2d183da4dab
happyday <jsoibn@gmail.com>
2023-09-22 10:50:02 +0700

    ai-generate-page

--- src/component/GeneratePage/AIGeneration.jsx
+++ src/component/GeneratePage/AIGeneration.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function AIGeneration() {
-  return <div>AIGeneration</div>;
+  return (
+    <div className="py-2 px-4">
+      <h1 className="text-white font-bold text-2xl">AI Image Generation</h1>
+      <div className="mt-2 rounded-md ">
+        <input
+          className="w-full rounded-md bg-[#141c2d] border border-transparent focus:border-purple-500 focus:outline-none py-4 px-4 text-sm"
+          type="text"
+          placeholder="Type a prompt..."
+        />
+      </div>
+      <div className="mt-4 gradient rounded-md w-32 h-11 flex justify-center items-center">
+        <button className="bg-gray-700 text-white text-base px-7 py-2 rounded-md">
+          Generate
+        </button>
+      </div>
+
+      <hr className="mt-4" />
+    </div>
+  );
 }
 
 export default AIGeneration;
--- src/layouts/FeaturesLayout.jsx
+++ src/layouts/FeaturesLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 function FeaturesLayout() {
   return (
     <div className="w-full relative flex justify-start">
-      <div className="w-[15%] h-screen fixed top-0 left-0 overflow-y-auto">
+      <div className="w-[15%] h-screen fixed top-0 left-0 overflow-y-auto border-r border-gray-100">
         <FeatureNavbar />
       </div>
       <div className="w-[85%] absolute top-0 right-0 py-4 px-4">
